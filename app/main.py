d196fa3069529c67e91dafbc1d7a3f3ac2e7b0ce
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2023-09-05 16:48:48 +0200

    Read All Todos

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from typing import Annotated, Generator
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 from app import models
-from app.database import engine
+from app.database import engine, SessionLocal
+from app.models import Todos
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+async def read_all(db: Annotated[Session, Depends(get_db)]):
+    return db.query(Todos).all()

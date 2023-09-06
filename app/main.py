67d0712de1b16441af77144b9b00113f9980f51a
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2023-09-06 11:04:26 +0200

    Read todo by id with path parameter

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
-from typing import Annotated, Generator
-from fastapi import FastAPI, Depends
+from typing import Annotated
+from fastapi import FastAPI, Depends, Path, HTTPException
 from sqlalchemy.orm import Session
+from starlette import status
+
 from app import models
 from app.database import engine, SessionLocal
 from app.models import Todos
@@ -18,6 +20,17 @@ def get_db():
         db.close()
 
 
-@app.get("/")
-async def read_all(db: Annotated[Session, Depends(get_db)]):
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def read_all(db: db_dependency):
     return db.query(Todos).all()
+
+
+@app.get("/todos/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail='Todo not found.')

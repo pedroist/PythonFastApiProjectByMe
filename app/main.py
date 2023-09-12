80475f509f4b7b9eb129362456137dd934280775
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2023-09-12 21:45:14 +0200

    Exercise: users route with get_user and change_password

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 
 from app import models
 from app.database import engine
-from app.routers import auth, todos, admin
+from app.routers import auth, todos, admin, users
+
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
@@ -10,3 +11,4 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
+app.include_router(users.router)

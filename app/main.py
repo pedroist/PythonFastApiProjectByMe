84d602d429e37d0c0a1df5b2e3852b31a6fc6944
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2023-09-12 17:40:18 +0200

    Admin router with read_all and delete endpoints

--- app/main.py
+++ app/main.py
@@ -2,12 +2,11 @@ from fastapi import FastAPI
 
 from app import models
 from app.database import engine
-from app.routers import auth
-from app.routers import todos
-
+from app.routers import auth, todos, admin
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
+app.include_router(admin.router)

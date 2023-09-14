73fd63d0b43ca9aaefc9ff66aca8c3092e273d40
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2023-09-14 15:39:18 +0200

    alembic revision -m "create phone number for user col"

---
+++ alembic/versions/f116584e3577_create_phone_number_for_user_col.py
@@ -0,0 +1,26 @@
+"""create phone number for user col
+
+Revision ID: f116584e3577
+Revises: 
+Create Date: 2023-09-14 15:28:36.840072
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'f116584e3577'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass

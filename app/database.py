5d2ebbaa70a5c969230772022af480295ff1d560
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2023-09-14 14:57:48 +0200

    using postgres again

--- app/database.py
+++ app/database.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:test1234!@localhost/TodoApplicationDatabase"
+#SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:<D****022$>@127.0.0.1:3306/TodoApplicationDatabase"
+# mysql takes the password defined for mysql on the mysql workbench
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL

17c4b979dd0a4fa9a85f7e862e2605a384430179
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2023-09-14 16:17:45 +0200

    updated models.py

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
 from app.database import Base
 
 
@@ -13,6 +15,11 @@ class Users(Base):
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
     role = Column(String)
+    phone_number = Column(String)
+    address_id = Column(Integer, ForeignKey("address.id"), nullable=True)
+
+    todos = relationship("Todos", back_populates="owner")
+    address = relationship("Address", back_populates="user_address")
 
 
 class Todos(Base):
@@ -24,3 +31,19 @@ class Todos(Base):
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
     owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("Users", back_populates="todos")
+
+
+class Address(Base):
+    __tablename__ = "address"
+
+    id = Column(Integer, primary_key=True, index=True)
+    address1 = Column(String)
+    address2 = Column(String)
+    city = Column(String)
+    state = Column(String)
+    country = Column(String)
+    postalcode = Column(String)
+
+    user_address = relationship("Users", back_populates="address")
